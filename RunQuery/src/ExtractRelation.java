9b237063ca7840288988febc74711de6a59978a5
Aju Thalappillil Scaria <ajuts@stanford.edu>
2013-04-23 14:10:08 -0700

    Removing jar files not needed

--- RunQuery/src/ExtractRelation.java
+++ RunQuery/src/ExtractRelation.java
@@ -3,6 +3,7 @@ import java.util.Properties;
 import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.trees.TreeCoreAnnotations;
 
 
 public class ExtractRelation {
@@ -17,7 +18,7 @@ public class ExtractRelation {
 	public String findRelation(String sentence, String ent1, String ent2) {
 		Annotation document = new Annotation(sentence);
 		processor.annotate(document);
-		
+		System.out.println(document.get(SentencesAnnotation.class).get(0).get(TreeCoreAnnotations.TreeAnnotation.class));
 		return "";
 	}
 	
