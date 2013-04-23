c1cd9d6520ab1ba16a61dae9a09aae7ab2323d8a
Aju Thalappillil Scaria <ajuts@stanford.edu>
2013-04-22 22:23:09 -0700

    formatting

--- RunQuery/src/Query.java
+++ RunQuery/src/Query.java
@@ -1,32 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import lemurproject.indri.QueryEnvironment;
 import lemurproject.indri.QueryRequest;
 import lemurproject.indri.QueryResult;
 import lemurproject.indri.QueryResults;
 
 public class Query {
-	
-	public Query() {
-		
+	final String INDEX_LOCATION;
+	QueryEnvironment env = new QueryEnvironment();
+	public Query(String IndexLocation) {
+		this.INDEX_LOCATION = IndexLocation;
+		try {
+			env.addIndex(INDEX_LOCATION);
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
-	public void queryIndex(String path, String query) {
-		QueryEnvironment env = new QueryEnvironment();
+	public List<String> queryIndex(String query, int resultsRequested) {
+		List<String> results = new ArrayList<String>();
 		try {
-			env.addIndex(path);
 			QueryRequest queryRequest = new QueryRequest();
 			queryRequest.query = query;
-			queryRequest.resultsRequested = 10;
+			queryRequest.resultsRequested = resultsRequested;
 			QueryResults queryResults = env.runQuery(queryRequest);
 			for(QueryResult result : queryResults.results) {
-				System.out.println(result.documentName);
+				results.add(result.documentName);
 			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return results;
 	}
 	
+	public List<String> queryIndex(String query) {
+		return queryIndex(query, Integer.MAX_VALUE);
+	}
+
 	public static void main(String[] args) {
-		new Query().queryIndex("/home/aju/cs341/data/index", "#od10(#1(Bill Gates) founded Microsoft)");
+		System.out.println(new Query("/home/aju/cs341/data/index").queryIndex("#od10(#1(Bill Gates) founded Microsoft)"));
 	}
 }
