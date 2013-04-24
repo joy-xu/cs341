39b2c436686cafcdfd7f9d64398836e1ea2295a2
Aju Thalappillil Scaria <ajuts@stanford.edu>
2013-04-23 20:56:08 -0700

    new jars

--- RunQuery/src/ExtractRelation.java
+++ RunQuery/src/ExtractRelation.java
@@ -3,15 +3,20 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 
-import org.apache.thrift.ShortStack;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.NodeList;
 
 import edu.stanford.nlp.ling.IndexedWord;
 import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import edu.stanford.nlp.trees.semgraph.SemanticGraph;
-import edu.stanford.nlp.trees.semgraph.SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation;
+import edu.stanford.nlp.semgraph.SemanticGraph;
+import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation;
+import edu.stanford.nlp.semgraph.SemanticGraphEdge;
 import edu.stanford.nlp.util.CoreMap;
+import edu.stanford.nlp.util.StringUtils;
 
 
 public class ExtractRelation {
@@ -23,28 +28,51 @@ public class ExtractRelation {
 		processor = new StanfordCoreNLP(props, false);
 	}
 	
-	public String findRelation(String sentence, String ent1, String ent2) {
+	public void findRelationsFromFile(String fileName, String ent1, String ent2){
+		org.w3c.dom.Document document;
+        // Make an  instance of the DocumentBuilderFactory
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        try {
+            // use the factory to take an instance of the document builder
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            // parse using the builder to get the DOM mapping of the    
+            // XML file
+            document = db.parse(fileName);
+
+            NodeList sentenceList = document.getElementsByTagName("SENTENCE");
+            System.out.println(String.format("%d sentences found matching '%s' and '%s'.", sentenceList.getLength(),
+            								ent1, ent2));// item(0).getTextContent());
+            for(int sentenceCounter = 0; sentenceCounter < sentenceList.getLength(); sentenceCounter++) {
+            	String sentence = sentenceList.item(sentenceCounter).getTextContent();
+    			System.out.println("Relation : " + findRelation(sentence, ent1, ent2));
+            }
+        }
+        catch(Exception ex) {
+        	
+        }
+	}
+	
+	public List<String> findRelation(String sentence, String ent1, String ent2) {
 		Annotation document = new Annotation(sentence);
 		processor.annotate(document);
 		CoreMap sentenceMap = document.get(SentencesAnnotation.class).get(0);
 		SemanticGraph graph = sentenceMap.get(CollapsedCCProcessedDependenciesAnnotation.class);
-		findRelation(graph, findWordsInSemanticGraph(graph, ent1), findWordsInSemanticGraph(graph, ent2));
-		  
-		return "";
+		return findRelation(graph, findWordsInSemanticGraph(graph, ent1), findWordsInSemanticGraph(graph, ent2));
 	}
 	
 	public List<IndexedWord> findWordsInSemanticGraph(SemanticGraph graph, String entity) {
 		List<String> entitySplits = Arrays.asList(entity.split(" "));
 		List<IndexedWord> words = new ArrayList<IndexedWord>();
 		for(IndexedWord word: graph.vertexSet()) {
-			if(entitySplits.contains(word.originalText()))
+			if(StringUtils.containsIgnoreCase(entitySplits, word.originalText()))
 				words.add(word);
 		}
 		
 		return words;
 	}
 	
-	public String findRelation(SemanticGraph graph, List<IndexedWord> words1, List<IndexedWord> words2) {
+	public List<String> findRelation(SemanticGraph graph, List<IndexedWord> words1, List<IndexedWord> words2) {
+		List<String> relationsFound = new ArrayList<String>();
 		List<IndexedWord> shortestPath = new ArrayList<IndexedWord>();
 		for(IndexedWord w1: words1) {
 			for(IndexedWord w2: words2) {
@@ -56,12 +84,22 @@ public class ExtractRelation {
 		}
 		
 		if(shortestPath.size() == 1) {
-			System.out.println("Relation : " + shortestPath.get(0).originalText());
-			return shortestPath.get(0).originalText();
+			relationsFound.add(shortestPath.get(0).originalText());
+			for(SemanticGraphEdge edge:graph.getIncomingEdgesSorted(shortestPath.get(0))) {
+				if(edge.getRelation().toString().equals("conj_and"))
+					relationsFound.add(edge.getSource().originalText());
+					
+			}
+			for(SemanticGraphEdge edge:graph.getOutEdgesSorted(shortestPath.get(0))) {
+				if(edge.getRelation().toString().equals("conj_and"))
+					relationsFound.add(edge.getTarget().originalText());
+					
+			}
+			return relationsFound;
 		}
 		else {
-			System.out.println(shortestPath);
-			return "";
+			System.out.println("Not returning anything from findRelation - " + shortestPath);
+			return relationsFound;
 		}
 	}
 }
