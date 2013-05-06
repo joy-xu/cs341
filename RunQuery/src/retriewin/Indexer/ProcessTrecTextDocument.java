ff5bcccf1c457bfd3f5b089492fb9d01ccde7e89
Aju T Scaria <ajutscaria@gmail.com>
2013-05-06 15:28:23 -0700

    Adding

---
+++ RunQuery/src/retriewin/Indexer/ProcessTrecTextDocument.java
@@ -0,0 +1,13 @@
+package retrieWin.Indexer;
+
+import java.util.List;
+
+public class ProcessTrecTextDocument {
+	public static List<String> extractRelevantSentences(List<TrecTextDocument> documents, String entity1, String entity2) {
+		return null;
+	}
+	
+	public static List<String> extractRelevantSentences(List<TrecTextDocument> documents, String entity1) {
+		return null;
+	}
+}
