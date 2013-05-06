756d7a303450138276c02a9b8b4017711b606dfb
Aju T Scaria <ajutscaria@gmail.com>
2013-05-06 15:46:11 -0700

    changes

---
+++ RunQuery/src/retriewin/Querying/ExecuteQuery.java
@@ -0,0 +1,99 @@
+package retrieWin.Querying;
+
+import java.util.*;
+import java.io.*;
+
+import retrieWin.Indexer.ThriftReader;
+import retrieWin.Indexer.TrecTextDocument;
+
+import lemurproject.indri.QueryEnvironment;
+import lemurproject.indri.QueryRequest;
+import lemurproject.indri.QueryResult;
+import lemurproject.indri.QueryResults;
+
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import fig.basic.LogInfo;
+
+import util.*;
+public class ExecuteQuery {
+	
+	final String INDEX_LOCATION;
+	QueryEnvironment env = new QueryEnvironment();
+	public ExecuteQuery(String IndexLocation) {
+		this.INDEX_LOCATION = IndexLocation;
+		try {
+			env.addIndex(INDEX_LOCATION);
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public List<String> queryIndex(String query, int resultsRequested) {
+		List<String> results = new ArrayList<String>();
+		try {
+			QueryRequest queryRequest = new QueryRequest();
+			queryRequest.query = query;
+			queryRequest.resultsRequested = resultsRequested;
+			QueryResults queryResults = env.runQuery(queryRequest);
+			for(QueryResult result : queryResults.results) {
+				results.add(result.documentName);
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return results;
+	}
+	
+	public List<String> queryIndex(String query) {
+		return queryIndex(query, Integer.MAX_VALUE);
+	}
+
+	/**
+	 * @param args
+	 */
+	public List<String> executeQuery(String indexLoc, String query, int numResults, String first, String second, String workingDirectory) {
+    	List<String> queryResults = queryIndex(query, numResults);
+    	Map<String, Set<String>> fileMap = new HashMap<String, Set<String>>();
+    	Map<String, Set<String>> streamIDMap = new HashMap<String, Set<String>>();
+    	
+    	for(String result:queryResults) {    	
+	    	String[] a = result.split("_");
+			String streamID = a[0];
+			String localfilename = a[1];
+			String[] b = localfilename.split("/");
+			String filename = b[b.length-1];
+			
+			String timeStamp = a[2];
+			String timeTokens[] = timeStamp.split("T");
+			String date = timeTokens[0];
+			String time = timeTokens[1];
+			String folder = timeTokens[0] + "-" + timeTokens[1].substring(0,2);
+			
+			if(!fileMap.keySet().contains(folder)) {
+				fileMap.put(folder, new HashSet<String>());
+			}
+			Set<String> set = fileMap.get(folder);
+			set.add(filename);
+			fileMap.put(folder, set);
+			
+			if(!streamIDMap.keySet().contains(filename)) {
+				streamIDMap.put(filename, new HashSet<String>());
+			}
+			set = streamIDMap.get(filename);
+			set.add(streamID);
+			streamIDMap.put(filename, set);
+    	}
+    	
+    	System.out.println("Retreiving documents...");
+    	
+    	List<TrecTextDocument> result = new ArrayList<TrecTextDocument>();
+    	for(String folder:fileMap.keySet()) {
+    		for(String file:fileMap.get(folder)) {
+				result.addAll(ThriftReader.GetFilteredFiles(folder, file, workingDirectory, streamIDMap.get(file)));
+    		}
+    	}
+    	return null;
+	}
+}
---
+++ RunQuery/src/retriewin/Querying/QueryBuilder.java
@@ -0,0 +1,49 @@
+package retrieWin.Querying;
+
+import java.util.List;
+
+public class QueryBuilder {
+	public static String buildOrderedQuery(String first, String second, int withinWords) {
+		String queryString = null;
+		if (second != null)
+			queryString = "#od" + withinWords + "(#1(" + first + ") #1(" + second + "))";
+		else
+			queryString = "#1(" + first + ")";
+		return queryString;
+	}
+	
+	public static String buildUnorderedQuery(String first, String second, int withinWords) {
+		String queryString = null;
+		if (second != null)
+			queryString = "#uw" + withinWords + "(#1(" + first + ") #1(" + second + "))";
+		else
+			queryString = "#1(" + first + ")";
+		return queryString;
+	}
+	
+	public static String buildSingleTermQuery(String term)
+	{
+		term = term.replaceAll("[^A-Za-z0-9 ]", "");
+		if(term.contains(",")) {
+			return String.format("#%d(%s)", 2, term.replace(",", ""));
+		}
+		return "#1(" + term + ")";
+	}
+	
+	public static String buildOrQuery(List<String> equivalents)
+	{
+		StringBuffer sbuf = new StringBuffer();
+		sbuf.append("<");
+		for (String s:equivalents)
+		{
+			sbuf.append("#5(");
+			sbuf.append(s);
+			sbuf.append(")");
+			sbuf.append(" ");
+		}
+		sbuf.deleteCharAt(sbuf.length()-1);
+		sbuf.append(">");
+		return sbuf.toString();
+	}
+}
+
