661c378433aaf86a9cf173d198ff079cceb13feb
Aju Thalappillil Scaria <ajuts@stanford.edu>
2013-05-07 01:12:06 -0700

    indexer

--- RunQuery/src/retrieWin/Indexer/IndriIndexBuilder.java
+++ RunQuery/src/retrieWin/Indexer/IndriIndexBuilder.java
@@ -1,16 +1,16 @@
 package retrieWin.Indexer;
 import lemurproject.indri.IndexEnvironment;
 
-
 public class IndriIndexBuilder {
 	public static void buildIndex(String indexLocation, String corpusLocation) {
 		IndexEnvironment index = new IndexEnvironment();
 		try {
-			
-			String[] fields = {"Time", "Text"};
+			String[] fields = {"TIME", "TEXT"};
 			index.setDocumentRoot(corpusLocation);
+			index.setStoreDocs(false);
 			index.setStemmer("krovetz");
 			index.setIndexedFields(fields);
+			index.setNumericField("TIME", true,"DateFieldAnnotator");
 			index.setMemory(1000000);
 			index.create(indexLocation);
 			index.close();
